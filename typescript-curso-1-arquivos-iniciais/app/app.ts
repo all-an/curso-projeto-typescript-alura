2e5fd6790a7450ee7c9c889dc32350f7a8d80e9e
all-an <all.ancapistan@gmail.com>
2021-11-14 14:06:00 -0300

    learning

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,5 +1,11 @@
 import { Negociacao } from "./models/negociacao.js";
+
 const negociacao = new Negociacao(new Date(), 10, 100);
+
 console.log(negociacao.data);
+
 //negociacao.quantidade = 1000;
+
 console.log(negociacao.voluime);
+
+
